a0db27e80ba8c0a3a37499c96911035e6fd78069
ErickOsla <automaster502@gmail.com>
2024-05-04 13:45:42 -0600

    test

--- src/Components/Menu/menu.js
+++ src/Components/Menu/menu.js
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-//import NavDropdown from 'react-bootstrap/NavDropdown';
+///import NavDropdown from 'react-bootstrap/NavDropdown';
 import './menu.scss'
 import { useSelector, useDispatch} from 'react-redux'
 import {changeOption} from '../../reducers/optionSlice'
