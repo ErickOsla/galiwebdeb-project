61e638f725d2eb336063db372c86a7822d4d15c9
ErickOsla <automaster502@gmail.com>
2024-05-13 18:00:32 -0600

    frontend and backend apps

---
+++ backend/routes/goals.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+let goals = [];
+
+router.get('/getGoals', function(req, res,next){
+    res.json(goals)
+})
+
+router.post('/addGoal', function(req, res,next){
+    let timestamp =Date.now() + Math.random();
+    if(req.body && req.body.name && 
+        req.body.description && req.body.dueDate){
+        req.body.id = timestamp.toString();
+        goals.push(req.body)
+        res.status(200).json(goals)
+    }else{
+        res.status(400).json({})
+    }
+});
+
+router.delete('/removeGoal/:id', function(req, res,next){
+    if(req.params && req.params.id){
+        let id = req.params.id;
+        goals = goals.filter(goal => goal.id !== id);
+        res.status(200).json(goals);
+    }else{
+        res.status(400).json({})
+    }
+})
+
+
+module.exports = router;
+
